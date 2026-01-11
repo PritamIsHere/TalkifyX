a51a50443a3386efb07bc819e5a12f6f5b60ddec
PritamIsHere <pritamkar98765@gmail.com>
2026-01-11 17:03:11 +0530

    Status page connection to backend

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h3059t43u68"
+    "revision": "0.v5u02u97dv8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
